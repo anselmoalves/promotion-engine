12aaa9ac067b4d6f112b3dc115d831163d30ace0
Anselmo Alves <me@anselmoalves.com>
2021-02-22 21:30:25 +0100

    Add test for the simplest scenario

---
+++ src/test/java/com/promotion/engine/AppTest.java
@@ -0,0 +1,27 @@
+package com.promotion.engine;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+
+    private Cart cart;
+
+    @BeforeEach
+    public void init() {
+        cart = new Cart();
+    }
+
+    @Test
+    public void scenarioA() {
+        cart.add(new Item(new Product(SKU.A), BigDecimal.ONE));
+        cart.add(new Item(new Product(SKU.B), BigDecimal.ONE));
+        cart.add(new Item(new Product(SKU.C), BigDecimal.ONE));
+
+        assertEquals(BigDecimal.valueOf(100), cart.total());
+    }
+}
