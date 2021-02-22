fb335a88a1930b7844520fcd0c7a255f4b13eec6
Anselmo Alves <me@anselmoalves.com>
2021-02-22 22:16:40 +0100

    Make the Scenario C test case pass

--- src/main/java/com/promotion/engine/rules/RuleCD.java
+++ src/main/java/com/promotion/engine/rules/RuleCD.java
@@ -10,8 +10,17 @@ import java.util.Map;
 @NoArgsConstructor
 public class RuleCD implements Rule {
 
+    private static final BigDecimal UNIT_PRICE_C = BigDecimal.valueOf(20);
+    private static final BigDecimal UNIT_PRICE_D = BigDecimal.valueOf(15);
+    private static final BigDecimal FIXED_CD_PRICE = BigDecimal.valueOf(30);
+
     @Override
     public BigDecimal apply(Map<SKU, Item> items) {
-        return BigDecimal.valueOf(20);
+        BigDecimal quantityC = items.getOrDefault(SKU.C, Item.of(SKU.C).withQuantity(BigDecimal.ZERO)).getQuantity();
+        BigDecimal quantityD = items.getOrDefault(SKU.D, Item.of(SKU.D).withQuantity(BigDecimal.ZERO)).getQuantity();
+
+        return quantityC.min(quantityD).multiply(FIXED_CD_PRICE)
+                .add(quantityC.subtract(quantityC.min(quantityD)).multiply(UNIT_PRICE_C))
+                .add(quantityD.subtract(quantityC.min(quantityD)).multiply(UNIT_PRICE_D));
     }
 }
